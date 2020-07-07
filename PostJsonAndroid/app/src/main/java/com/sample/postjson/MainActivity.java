04d937b067c00ae2f4093ed21c0c90bed6980224
정한결 <han1g@flyhigh-x.com>
2020-07-07 16:12:30 +0900

    commit

---
+++ PostJsonAndroid/app/src/main/java/com/sample/postjson/MainActivity.java
@@ -0,0 +1,198 @@
+package com.sample.postjson;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvIsConnected, tvResponse;
+    EditText etServerURL;
+    EditText etMsg;
+    Button btnPost;
+    static    String strJson = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get reference to the views
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+        etServerURL = findViewById(R.id.etServerUrl);
+        etMsg = (EditText) findViewById(R.id.etMsg);
+        btnPost = (Button) findViewById(R.id.btnPost);
+        tvResponse = (TextView) findViewById(R.id.tvResponse);
+
+        // check if you are connected or not
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
+        }
+
+        // add click listener to Button "POST"
+        btnPost.setOnClickListener(this);
+
+    }
+
+    public static String POST(String url, String msg){
+        InputStream is = null;
+        String result = "";
+        try {
+            URL urlCon = new URL(url);
+            HttpURLConnection httpCon = (HttpURLConnection)urlCon.openConnection();
+
+            String json = "";
+
+            // build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("message", msg );
+
+            // convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // Set some headers to inform server about the type of the content
+            httpCon.setRequestMethod("POST");
+            httpCon.setRequestProperty("Accept", "application/json");
+            httpCon.setRequestProperty("Content-type", "application/json");
+
+            // OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
+            httpCon.setDoOutput(true);
+            // InputStream으로 서버로 부터 응답을 받겠다는 옵션.
+            httpCon.setDoInput(true);
+
+            OutputStream os = httpCon.getOutputStream();
+            os.write(json.getBytes("utf-8"));
+            os.flush();
+            // receive response as inputStream
+            try {
+                is = httpCon.getInputStream();
+                // convert inputstream to string
+                if(is != null)
+                    result = convertInputStreamToString(is);
+                else
+                    result = "Did not work!";
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                httpCon.disconnect();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnPost:
+                if(!validate())
+                    Toast.makeText(getBaseContext(), "Enter some data!", Toast.LENGTH_LONG).show();
+                else {
+                    // call AsynTask to perform network operation on separate thread
+                    HttpAsyncTask httpTask = new HttpAsyncTask(MainActivity.this);
+                    // SERVERURL:연결할 서버
+                    httpTask.execute("http://127.0.0.1:8080/", etMsg.getText().toString());
+                }
+                break;
+        }
+
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        private   MainActivity mainAct;
+
+        HttpAsyncTask(MainActivity mainActivity) {
+            this.mainAct = mainActivity;
+        }
+        @Override
+        protected String doInBackground(String... urls) {
+            //urls[0] : url
+            //urls[1~]: msg
+
+            return POST(urls[0],urls[1]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            strJson = result;
+            mainAct.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(mainAct, "Received!", Toast.LENGTH_LONG).show();
+                    try {
+                        JSONArray json = new JSONArray(strJson);
+                        mainAct.tvResponse.setText(json.toString(1));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private boolean validate(){
+        if(etMsg.getText().toString().trim().equals(""))
+            return false;
+        else
+            return true;
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+}
