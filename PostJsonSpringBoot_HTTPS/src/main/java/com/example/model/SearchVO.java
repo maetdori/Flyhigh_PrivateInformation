c6d5498a145ada052619db8c9ffdabb76c902706
이해인 <odong2@flyhigh-x.com>
2020-07-08 10:43:46 +0900

    push PostJsonSpringBoot_HTTPS

---
+++ PostJsonSpringBoot_HTTPS/src/main/java/com/example/model/SearchVO.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+public class SearchVO {
+	private String name;
+	private String sex;
+	private String id;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+}
